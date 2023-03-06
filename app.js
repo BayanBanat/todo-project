971a2e3ad76c6fd908057c639ea462282d8a2b86
BayanBanat <bayankbanat99@gmail.com>
2023-03-06 11:43:17 +0000

    modiefing

--- app.js
+++ app.js
@@ -1,36 +1,53 @@
 'use strict';
 var welcomingMessage;
-var userName=prompt("What is your name?");
+var userName = prompt("What is your name?");
 console.log(userName);
-if (userName==""){
-    alert("invalid");
-};
-var gender=prompt("Inter your gender: male / female");
+
+var gender = prompt("Inter your gender: male / female");
 console.log(gender);
-if (gender==""){
-    alert("invalid");
-};
-var age=prompt("Inter your age:");
-if ( age==""){
-    alert("invalid");
-}else if(age<=0 ){
+
+var age = prompt("Inter your age:");
+if (age <= 0) {
     alert("your age less than or equal zero!!!")
 };
 
-var skip=confirm("Are you want to skip the welcoming message!!");
-if(gender== "male"){
-alert("Mr "+userName) 
+var skip = confirm("Are you want to skip the welcoming message!!");
+if (gender == "male") {
+    alert("Mr " + userName)
 }
-else if (gender=="female"){
-    alert("Ms "+userName)
+else if (gender == "female") {
+    alert("Ms " + userName)
 }
-else{
-    alert(userName) 
+else {
+    alert(userName)
 };
-let Exit=confirm("Are you sure to exit of this page?");
-let learning=confirm("Are you a fast learner person?");
-let intersting=confirm("Are you interested in learning programming?");
-let answers=["bayan","raneim","abdullah","male","female",15,18,19,20,25,33];
-for(let i=0;i<answers.length;i++){
-    console.log(answers[i]);
+
+
+//lab 6//
+
+
+let userAnswer = [];
+
+function asks(question) {
+    return prompt(question);
 };
+function answersSaving(arr) {
+
+    let Exit = asks("Are you sure to exit of this page?");
+    let learning = asks("Are you a fast learner person?");
+    let intersting = asks("Are you interested in learning programming?");
+    arr.push(Exit, learning, intersting);
+};
+
+
+function validating(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == "") {
+            arr[i] = "invalid";
+
+        }
+    }
+};
+asks(userAnswer);
+answersSaving(userAnswer);
+validating(userAnswer);
