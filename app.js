8c71a61c57846f65ad7397ea9ce79ab8b141c237
BayanBanat <bayankbanat99@gmail.com>
2023-03-08 12:40:08 +0000

    new solving

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 'use strict';
-var welcomingMessage;
-var userName = prompt("What is your name?");
-console.log(userName);
+// var welcomingMessage;
+// var userName = prompt("What is your name?");
+// console.log(userName);
 
-var gender = prompt("Inter your gender: male / female");
-console.log(gender);
+// var gender = prompt("Inter your gender: male / female");
+// console.log(gender);
 
-var age = prompt("Inter your age:");
-if (age <= 0) {
-    alert("your age less than or equal zero!!!")
-};
+// var age = prompt("Inter your age:");
+// if (age <= 0) {
+//     alert("your age less than or equal zero!!!")
+// };
 
-var skip = confirm("Are you want to skip the welcoming message!!");
-if (gender == "male") {
-    alert("Mr " + userName)
-}
-else if (gender == "female") {
-    alert("Ms " + userName)
-}
-else {
-    alert(userName)
-};
+// var skip = confirm("Are you want to skip the welcoming message!!");
+// if (gender == "male") {
+//     alert("Mr " + userName)
+// }
+// else if (gender == "female") {
+//     alert("Ms " + userName)
+// }
+// else {
+//     alert(userName)
+// };
 
 
 //lab 6//
@@ -48,6 +48,14 @@ function validating(arr) {
         }
     }
 };
+
+function printer(arr){
+    for( let i=0;i<arr.length;i++){
+        console.log(arr[i])
+    }
+
+}
 asks(userAnswer);
 answersSaving(userAnswer);
 validating(userAnswer);
+printer(userAnswer);
