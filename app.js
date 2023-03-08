c152d9b436aeb3f0593652e2768f8957d9556c6b
BayanBanat <bayankbanat99@gmail.com>
2023-03-08 12:45:39 +0000

    finally

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 'use strict';
-// var welcomingMessage;
-// var userName = prompt("What is your name?");
-// console.log(userName);
+//lab 5
+var welcomingMessage;
+var userName = prompt("What is your name?");
+console.log(userName);
 
-// var gender = prompt("Inter your gender: male / female");
-// console.log(gender);
+var gender = prompt("Inter your gender: male / female");
+console.log(gender);
 
-// var age = prompt("Inter your age:");
-// if (age <= 0) {
-//     alert("your age less than or equal zero!!!")
-// };
+var age = prompt("Inter your age:");
+if (age <= 0) {
+    alert("your age less than or equal zero!!!")
+};
 
-// var skip = confirm("Are you want to skip the welcoming message!!");
-// if (gender == "male") {
-//     alert("Mr " + userName)
-// }
-// else if (gender == "female") {
-//     alert("Ms " + userName)
-// }
-// else {
-//     alert(userName)
-// };
+var skip = confirm("Are you want to skip the welcoming message!!");
+if (gender == "male") {
+    alert("Mr " + userName)
+}
+else if (gender == "female") {
+    alert("Ms " + userName)
+}
+else {
+    alert(userName)
+};
 
 
 //lab 6//
